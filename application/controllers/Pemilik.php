51b178d691bb12995afb5c7df44678f2ec8d829a
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-05-13 07:42:56 +0700

    menambahkan proses pemilik laporan dan kelola user

---
+++ application/controllers/Pemilik.php
@@ -0,0 +1,33 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Pemilik extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('m_admin');
+        $this->load->model('m_pesanan_masuk');
+        $this->load->model('m_transaksi');
+        $this->load->model('m_produk');
+        $this->load->model('m_chatting');
+    }
+
+    // List all your items
+    public function index()
+    {
+        $data = array(
+            'title' => 'Dashboard',
+            'total_produk' => $this->m_admin->total_produk(),
+            'total_pesanan' => $this->m_admin->total_pesanan(),
+            'total_pelanggan' => $this->m_admin->total_pelanggan(),
+            'total_transaksi' => $this->m_admin->total_transaksi(),
+            'grafik' => $this->m_transaksi->grafik(),
+            'isi' => 'v_pemilik'
+        );
+        $this->load->view('layout/pemilik/v_wrapper', $data, FALSE);
+    }
+}
